a2f319cee4ad84b16c2beb7019008b1f7b3e0c5b
AdamGhieh <ghiehadam123@gmail.com>
2025-04-27 18:44:39 -0400

    Added SRA and SLTU operations to ALU

--- ALU/ALU.vhd
+++ ALU/ALU.vhd
@@ -6,15 +6,27 @@ entity alu is
 	port
 	(
 		A, B : in std_logic_vector(31 downto 0);
-		ALU_ctrl : in std_logic_vector(2 downto 0);
-		overflow, negative, zero : out std_logic;
+		ALU_ctrl : in std_logic_vector(3 downto 0);
+		overflow, negative, zero, carry : out std_logic;
 		result : out std_logic_vector(31 downto 0)
 	);
 end alu;
 
 architecture behave of alu is
 
+	constant ADDop : std_logic_vector(3 downto 0) := "0000";
+	constant SUBop : std_logic_vector(3 downto 0) := "0001";
+	constant ANDop : std_logic_vector(3 downto 0) := "0010";
+	constant ORop : std_logic_vector(3 downto 0) := "0011";
+	constant XORop : std_logic_vector(3 downto 0) := "0100";
+	constant SLLop : std_logic_vector(3 downto 0) := "0101";
+	constant SRLop : std_logic_vector(3 downto 0) := "0110";
+	constant SLTop : std_logic_vector(3 downto 0) := "0111";
+	constant SLTUop : std_logic_vector(3 downto 0) := "1000";
+	constant SRAop : std_logic_vector(3 downto 0) := "1001";
+	
 	signal Temp_Result : std_logic_vector(32 downto 0);
+	signal V : std_logic;
 	
 begin 
 	alu : process(A, B, ALU_ctrl) 
@@ -24,38 +36,38 @@ begin
 		
 		case ALU_ctrl is 
 		
-			when "000" =>	-- ADD Function --
+			when ADDop =>	-- ADD Function --
 				
 				Temp_Result <= std_logic_vector(signed('0' & A) + signed('0' & B));
 				
-			when "001" => -- SUB Function --
+			when SUBop | SLTop | SLTUop => -- SUB Function (used for SLT and SLTU as well) --
 			
-				Temp_Result <= std_logic_vector(signed('0' & A) - signed('0' & B));
+				Temp_Result <= std_logic_vector(signed('0' & A) + ('0' & (not signed(B) + 1)));
 			
-			when "010" =>	-- AND Function --
+			when ANDop =>	-- AND Function --
 			
 				Temp_Result(31 downto 0) <= A AND B;
 				
-			when "011" => -- OR Function --
+			when ORop => -- OR Function --
 			
 				Temp_Result(31 downto 0) <= A OR B;
 				
-			when "100" => -- XOR Function --
+			when XORop => -- XOR Function --
 			
 				Temp_Result(31 downto 0) <= A XOR B;
 				
-			when "101" => -- Logical Left Shift (SLL) --
+			when SLLop  => -- Logical Left Shift (SLL) -- 
 				
 				Temp_Result(31 downto 0) <= std_logic_vector(shift_left(unsigned(A), to_integer(unsigned(B))));
 			
-			when "110" => -- Logical Right Shift (SRL) --
+			when SRLop => -- Logical Right Shift (SRL) --
 				
 				Temp_Result(31 downto 0) <= std_logic_vector(shift_right(unsigned(A), to_integer(unsigned(B))));
 			
-			when "111"	 => -- Less Than (SLT) --
-			
-				Temp_Result <= std_logic_vector(signed('0' & A) - signed('0' & B));
-			
+			when SRAop => -- Arithmetic right shift (SRA) --
+				
+				Temp_Result(31 downto 0) <= std_logic_vector(shift_right(signed(A), to_integer(unsigned(B))));
+				
 			when others =>
 			
 				Temp_result <= (others => '0');
@@ -63,17 +75,33 @@ begin
 		end case;
 	end process;
 	
-		-- If less than option is chosen for ALU_ctrl, use LSB as sign bit, and set the rest to 0, otherwise use temp_result --
-		result <= (x"0000000" & "000" & Temp_Result(31)) when ALU_ctrl = "111"  else Temp_Result(31 downto 0);
+	process(A, B, Temp_Result, ALU_ctrl)
+	begin
 		
-		-- Overflow is calculated by comparing operands sign to result sign for arithmetic operation, overflow = 0 for any other operation --
-		overflow <= (A(31) and B(31) and not(Temp_Result(31))) or (not(A(31)) and not(B(31)) and Temp_Result(31)) when ALU_ctrl = "000" else
-		     (A(31) and not(B(31)) and not(Temp_Result(31))) or (not(A(31)) and B(31) and Temp_Result(31)) when ALU_ctrl = "001" else '0';
+		if(ALU_ctrl = ADDop) then
+		
+			V <= (A(31) and B(31) and not(Temp_Result(31))) or (not(A(31)) and not(B(31)) and Temp_Result(31));
+			
+		else
+		
+			V <= (A(31) and not(B(31)) and not(Temp_Result(31))) or (not(A(31)) and B(31) and Temp_Result(31));
+		
+		end if;
+	end process;
 	
-		-- Zero flag checks for result value --
-		zero <= '1' when to_integer(signed(Temp_Result(31 downto 0))) = 0 else '0';
+	-- If less than option is chosen for ALU_ctrl, use LSB as sign bit, and set the rest to 0, otherwise use temp_result, for SLTU, inverse of carry is used to tell sign --
+	result <= (x"0000000" & "000" & (Temp_Result(31) xor V)) when ALU_ctrl = SLTop else (x"0000000" & "000" & not Temp_Result(32)) when ALU_ctrl = SLTUop  else Temp_Result(31 downto 0);
 		
-		-- Signed bit of result --
-		negative <= '0' when ALU_ctrl = "111" else Temp_Result(31);
+	-- Overflow is calculated by comparing operands sign to result sign for arithmetic operation, overflow = 0 for any other operation --
+	overflow <= V when ALU_ctrl = ADDop or ALU_ctrl = SUBop else '0';
+	
+	-- Zero flag checks for result value --
+	zero <= '1' when to_integer(signed(Temp_Result(31 downto 0))) = 0 else '0';
+		
+	-- Signed bit of result --
+	negative <= Temp_Result(31) when ALU_ctrl = ADDop or ALU_ctrl = SUBop else '0';
+		
+	-- Carry is the extra bit --
+	carry <= Temp_Result(32)  when ALU_ctrl = ADDop or ALU_ctrl = SUBop else '0';
 		
 end behave;
--- ALU/ALU_tb.vhd
+++ ALU/ALU_tb.vhd
@@ -9,29 +9,30 @@ end alu_tb;
 architecture tb of alu_tb is
 
 	signal A, B : std_logic_vector(31 downto 0);
-	signal ALU_Control : std_logic_vector(2 downto 0);
-	signal V, N, Z : std_logic;
+	signal ALU_Control : std_logic_vector(3 downto 0);
+	signal V, N, Z, C : std_logic;
 	signal ALU_Result : std_logic_vector(31 downto 0);
 
 begin 
 	
-	ALU : entity work.alu(behave)
+	testALU : entity work.alu(behave)
 		port map
 		(
 			A => A,
 			B => B,
-			ALU_Control => ALU_Control,
-			V => V,
-			N => N,
-			Z => Z,
-			ALU_Result => ALU_Result
+			ALU_ctrl => ALU_Control,
+			overflow => V,
+			negative => N,
+			zero => Z,
+			result => ALU_Result,
+			carry => C
 		);
 		
 	process
 	begin
 	
 		-- Test ADD (No Overflow)
-		A <= X"7FFFFFFE"; B <= X"00000001"; ALU_Control <= "000"; 
+		A <= X"7FFFFFFE"; B <= X"00000001"; ALU_Control <= "0000"; 
 		
 		wait for 10 ns;
 		
@@ -39,10 +40,10 @@ begin
 		assert V = '0' report "Case 1: Failed (Invalid V flag)";
 		assert N = '0' report "Case 1: Failed (Invalid N flag)";
 		assert Z = '0' report "Case 1: Failed (Invalid Z flag)";
-		
+		assert C = '0' report "Case 1: Failed (Invalid C flag)";
 		
 		-- ADD Test (Overflow)
-		A <= X"7FFFFFFF"; B <= X"00000001"; ALU_Control <= "000"; 
+		A <= X"7FFFFFFF"; B <= X"00000001"; ALU_Control <= "0000"; 
 		
 		wait for 10 ns;
 		
@@ -50,10 +51,10 @@ begin
 		assert V = '1' report "Case 2: Failed (Invalid V flag)";
 		assert N = '1' report "Case 2: Failed (Invalid N flag)";
 		assert Z = '0' report "Case 2: Failed (Invalid Z flag)";
-		
+		assert C = '0' report "Case 2: Failed (Invalid C flag)";
 		
 		-- SUB Test (Negative)
-		A <= X"00000002"; B <= X"00000003"; ALU_Control <= "001"; 
+		A <= X"00000002"; B <= X"00000003"; ALU_Control <= "0001"; 
 		
 		wait for 10 ns;
 		
@@ -61,10 +62,10 @@ begin
 		assert V = '0' report "Case 3: Failed (Invalid V flag)";
 		assert N = '1' report "Case 3: Failed (Invalid N flag)";
 		assert Z = '0' report "Case 3: Failed (Invalid Z flag)";
-		
+		assert C = '0' report "Case 3: Failed (Invalid C flag)";
 		
 		-- Test Zero Flag
-		A <= X"00000005"; B <= X"00000005"; ALU_Control <= "001"; 
+		A <= X"00000005"; B <= X"00000005"; ALU_Control <= "0001"; 
 		
 		wait for 10 ns;
 		
@@ -72,10 +73,10 @@ begin
 		assert V = '0' report "Case 4: Failed (Invalid V flag)";
 		assert N = '0' report "Case 4: Failed (Invalid N flag)";
 		assert Z = '1' report "Case 4: Failed (Invalid Z flag)";
-		
+		assert C = '1' report "Case 4: Failed (Invalid C flag)";
 		
 		-- AND Test
-		A <= X"0000000F"; B <= X"00000007"; ALU_Control <= "010"; 
+		A <= X"0000000F"; B <= X"00000007"; ALU_Control <= "0010"; 
 		
 		wait for 10 ns;
 	
@@ -83,10 +84,10 @@ begin
 		assert V = '0' report "Case 5: Failed (Invalid V flag)";
 		assert N = '0' report "Case 5: Failed (Invalid N flag)";
 		assert Z = '0' report "Case 5: Failed (Invalid Z flag)";
-		
+		assert C = '0' report "Case 5: Failed (Invalid C flag)";
 		
 		-- OR Test
-		A <= X"0000000F"; B <= X"00000007"; ALU_Control <= "011"; 
+		A <= X"0000000F"; B <= X"00000007"; ALU_Control <= "0011"; 
 		
 		wait for 10 ns;
 		
@@ -94,10 +95,10 @@ begin
 		assert V = '0' report "Case 6: Failed (Invalid V flag)";
 		assert N = '0' report "Case 6: Failed (Invalid N flag)";
 		assert Z = '0' report "Case 6: Failed (Invalid Z flag)";
-	
+		assert C = '0' report "Case 6: Failed (Invalid C flag)";
 	
 		-- XOR Test
-		A <= X"0000000F"; B <= X"00000007"; ALU_Control <= "100";
+		A <= X"0000000F"; B <= X"00000007"; ALU_Control <= "0100";
 
 		wait for 10 ns;
 		
@@ -105,10 +106,10 @@ begin
 		assert V = '0' report "Case 7: Failed (Invalid V flag)";
 		assert N = '0' report "Case 7: Failed (Invalid N flag)";
 		assert Z = '0' report "Case 7: Failed (Invalid Z flag)";
-		
+		assert C = '0' report "Case 7: Failed (Invalid C flag)";
 		
 		-- Shift Left Test
-        A <= X"00000001"; B <= X"00000002"; ALU_Control <= "101"; 
+        A <= X"00000001"; B <= X"00000002"; ALU_Control <= "0101"; 
 		
 		wait for 10 ns;
 		
@@ -116,10 +117,10 @@ begin
 		assert V = '0' report "Case 8: Failed (Invalid V flag)";
 		assert N = '0' report "Case 8: Failed (Invalid N flag)";
 		assert Z = '0' report "Case 8: Failed (Invalid Z flag)";
-		
+		assert C = '0' report "Case 8: Failed (Invalid C flag)";
 		
         -- Shift Right Test
-        A <= X"00000008"; B <= X"00000002"; ALU_Control <= "110"; 
+        A <= X"00000008"; B <= X"00000002"; ALU_Control <= "0110"; 
 		
 		wait for 10 ns;
 		
@@ -127,10 +128,10 @@ begin
 		assert V = '0' report "Case 9: Failed (Invalid V flag)";
 		assert N = '0' report "Case 9: Failed (Invalid N flag)";
 		assert Z = '0' report "Case 9: Failed (Invalid Z flag)";
-		
+		assert C = '0' report "Case 9: Failed (Invalid C flag)";
 		
         -- SLT Test
-        A <= X"00000005"; B <= X"00000007"; ALU_Control <= "111"; 
+        A <= X"FFFFFFF5"; B <= X"00000007"; ALU_Control <= "0111"; 
 		
 		wait for 10 ns;
 		
@@ -138,9 +139,76 @@ begin
 		assert V = '0' report "Case 10: Failed (Invalid V flag)";
 		assert N = '0' report "Case 10: Failed (Invalid N flag)";
 		assert Z = '0' report "Case 10: Failed (Invalid Z flag)";
+		assert C = '0' report "Case 10: Failed (Invalid C flag)";
+		
+        A <= X"FF000000"; B <= X"F0000000"; ALU_Control <= "0111"; 
+		
+		wait for 10 ns;
+		
+		assert ALU_Result = x"00000000" report "Case 11: Failed (incorrect result)";
+		assert V = '0' report "Case 11: Failed (Invalid V flag)";
+		assert N = '0' report "Case 11: Failed (Invalid N flag)";
+		assert Z = '0' report "Case 11: Failed (Invalid Z flag)";
+		assert C = '0' report "Case 11: Failed (Invalid C flag)";
+		
+        -- With overflow 
+		
+		A <= X"7FFFFFFF"; B <= X"FFFFFFFF"; ALU_Control <= "0111"; 
+		
+		wait for 10 ns;
+		
+		assert ALU_Result = x"00000000" report "Case 12: Failed (incorrect result)";
+		assert V = '0' report "Case 12: Failed (Invalid V flag)";
+		assert N = '0' report "Case 12: Failed (Invalid N flag)";
+		assert Z = '0' report "Case 12: Failed (Invalid Z flag)";		
+		assert C = '0' report "Case 12: Failed (Invalid C flag)";
+		
+		-- SLTU Test
+        A <= X"FFFFFFF5"; B <= X"00000007"; ALU_Control <= "1000"; 
+		
+		wait for 10 ns;
+		
+		assert ALU_Result = x"00000000" report "Case 13: Failed (incorrect result)";
+		assert V = '0' report "Case 13: Failed (Invalid V flag)";
+		assert N = '0' report "Case 13: Failed (Invalid N flag)";
+		assert Z = '0' report "Case 13: Failed (Invalid Z flag)";
+		assert C = '0' report "Case 13: Failed (Invalid C flag)";
+		
+        A <= X"FF000000"; B <= X"F0000000"; ALU_Control <= "1000"; 
+		
+		wait for 10 ns;
+		
+		assert ALU_Result = x"00000000" report "Case 14: Failed (incorrect result)";
+		assert V = '0' report "Case 14: Failed (Invalid V flag)";
+		assert N = '0' report "Case 14: Failed (Invalid N flag)";
+		assert Z = '0' report "Case 14: Failed (Invalid Z flag)";
+		assert C = '0' report "Case 14: Failed (Invalid C flag)";
+        
+		A <= X"00000000"; B <= X"FFFFFFFB" ; ALU_Control <= "1000"; 
+		
+		wait for 10 ns;
+		
+		assert ALU_Result = x"00000001" report "Case 15: Failed (incorrect result)";
+		assert V = '0' report "Case 15: Failed (Invalid V flag)";
+		assert N = '0' report "Case 15: Failed (Invalid N flag)";
+		assert Z = '0' report "Case 15: Failed (Invalid Z flag)";
+		assert C = '0' report "Case 15: Failed (Invalid C flag)";
+		
+		-- SRA Test 
+		A <= X"FFFFFFF8"; B <= X"00000002"; ALU_Control <= "1001";
+		
+		wait for 10 ns;
+		
+		assert ALU_Result = x"FFFFFFFE" report "Case 16: Failed (incorrect result)";
+		assert V = '0' report "Case 16: Failed (Invalid V flag)";
+		assert N = '0' report "Case 16: Failed (Invalid N flag)";
+		assert Z = '0' report "Case 16: Failed (Invalid Z flag)";
+		assert C = '0' report "Case 16: Failed (Invalid C flag)";
 		
 		report  "Test Bench Complete";
 		wait;
+		
+
 		
 	end process;
 
