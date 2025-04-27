912905e1cf3c56fb1344d3af023f5a6979c7bc11
AdamGhieh <ghiehadam123@gmail.com>
2025-04-27 18:45:36 -0400

    Added control unit encodings for new instructions

--- Control Unit/control.vhd
+++ Control Unit/control.vhd
@@ -9,20 +9,22 @@ entity control is
 		op : in std_logic_vector(6 downto 0);
 		funct3 : in std_logic_vector(14 downto 12);
 		funct7 : in std_logic_vector(6 downto 0);
-		Zero : in std_logic;
+		Zero, Negative, Carry, Overflow : in std_logic;
 		
 		-- Output buses --
-		MemWrite, ALUSrc, RegWrite : out std_logic;
-		ResultSrc, PCSrc : out std_logic_vector(1 downto 0);
-		ALUControl, ImmSrc : out std_logic_vector(2 downto 0)
+		MemWrite, ALUSrc, RegWrite, DataType : out std_logic;
+		PCSrc, DataSize : out std_logic_vector(1 downto 0);
+		ImmSrc, ResultSrc : out std_logic_vector(2 downto 0);
+		ALUControl : out std_logic_vector(3 downto 0)
 	);
 end control;
 
 architecture behave of control is
 
-	signal MemWrite1, ALUSrc1, RegWrite1 : std_logic;
-	signal ResultSrc1, PCSrc1 : std_logic_vector(1 downto 0);
-	signal ALUControl1, ImmSrc1 : std_logic_vector(2 downto 0);
+	signal MemWrite1, ALUSrc1, RegWrite1, DataType1 : std_logic;
+	signal PCSrc1, DataSize1 : std_logic_vector(1 downto 0);
+	signal ImmSrc1, ResultSrc1 : std_logic_vector(2 downto 0);
+	signal ALUControl1 : std_logic_vector(3 downto 0);
 
 begin
 
@@ -35,82 +37,122 @@ begin
 		if op = "0110011" and funct3 = "000" and funct7 = "0000000" then	-- ADD --
 		
 			PCSrc1 <= "00";
-			ResultSrc1 <= "00";
+			ResultSrc1 <= "000";
 			MemWrite1 <= '0';
-			ALUControl1 <= "000";
+			ALUControl1 <= "0000";
 			ALUSrc1 <= '0';
 			ImmSrc1 <= "111"; 	-- Set Invalid Type so that Extend unit outputs 0s 
 			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
 			
 		elsif op = "0110011" and funct3 = "000" and funct7 = "0100000" then	-- SUB --
 		
 			PCSrc1 <= "00";
-			ResultSrc1 <= "00";
+			ResultSrc1 <= "000";
 			MemWrite1 <= '0';
-			ALUControl1 <= "001";
+			ALUControl1 <= "0001";
 			ALUSrc1 <= '0';
 			ImmSrc1 <= "111";	-- Set Invalid Type so that Extend unit outputs 0s 
 			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
 	
 		elsif op = "0110011" and funct3 = "111" and funct7 = "0000000" then -- AND -- 
 		
 			PCSrc1 <= "00";
-			ResultSrc1 <= "00";
+			ResultSrc1 <= "000";
 			MemWrite1 <= '0';
-			ALUControl1 <= "010";
+			ALUControl1 <= "0010";
 			ALUSrc1 <= '0';
 			ImmSrc1 <= "111";	-- Set Invalid Type so that Extend unit outputs 0s 
 			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
 		
 		elsif op = "0110011" and funct3 = "110" and funct7 = "0000000" then -- OR --
 		
 			PCSrc1 <= "00";
-			ResultSrc1 <= "00";
+			ResultSrc1 <= "000";
 			MemWrite1 <= '0';
-			ALUControl1 <= "011";
+			ALUControl1 <= "0011";
 			ALUSrc1 <= '0';
 			ImmSrc1 <= "111";	-- Set Invalid Type so that Extend unit outputs 0s 
 			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
 		
 		elsif op = "0110011" and funct3 = "100" and funct7 = "0000000" then -- XOR --
 		
 			PCSrc1 <= "00";
-			ResultSrc1 <= "00";
+			ResultSrc1 <= "000";
 			MemWrite1 <= '0';
-			ALUControl1 <= "100";
+			ALUControl1 <= "0100";
 			ALUSrc1 <= '0';
 			ImmSrc1 <= "111";	-- Set Invalid Type so that Extend unit outputs 0s 
 			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
 		
 		elsif op = "0110011" and funct3 = "001" and funct7 = "0000000" then -- SLL --
 		
 			PCSrc1 <= "00";
-			ResultSrc1 <= "00";
+			ResultSrc1 <= "000";
 			MemWrite1 <= '0';
-			ALUControl1 <= "101";
+			ALUControl1 <= "0101";
 			ALUSrc1 <= '0';
 			ImmSrc1 <= "111";	-- Set Invalid Type so that Extend unit outputs 0s 
 			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
 	
 		elsif op = "0110011" and funct3 = "101" and funct7 = "0000000" then -- SRL --
 		
 			PCSrc1 <= "00";
-			ResultSrc1 <= "00";
+			ResultSrc1 <= "000";
 			MemWrite1 <= '0';
-			ALUControl1 <= "110";
+			ALUControl1 <= "0110";
 			ALUSrc1 <= '0';
 			ImmSrc1 <= "111";	-- Set Invalid Type so that Extend unit outputs 0s 
 			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
 		
 		elsif op = "0110011" and funct3 = "010" and funct7 = "0000000" then -- SLT --
 		
 			PCSrc1 <= "00";
-			ResultSrc1 <= "00";
+			ResultSrc1 <= "000";
 			MemWrite1 <= '0';
-			ALUControl1 <= "111";
+			ALUControl1 <= "0111";
 			ALUSrc1 <= '0';
 			ImmSrc1 <= "111";	-- Set Invalid Type so that Extend unit outputs 0s 
 			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
+			
+		elsif op = "0110011" and funct3 = "101" and funct7 = "0100000" then -- SRA --
+		
+			PCSrc1 <= "00";
+			ResultSrc1 <= "000";
+			MemWrite1 <= '0';
+			ALUControl1 <= "1001";
+			ALUSrc1 <= '0';
+			ImmSrc1 <= "111";	-- Set Invalid Type so that Extend unit outputs 0s 
+			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
+			
+		elsif op = "0110011" and funct3 = "011" and funct7 = "0000000" then -- SLTU --
+		
+			PCSrc1 <= "00";
+			ResultSrc1 <= "000";
+			MemWrite1 <= '0';
+			ALUControl1 <= "1000";
+			ALUSrc1 <= '0';
+			ImmSrc1 <= "111";	-- Set Invalid Type so that Extend unit outputs 0s 
+			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
 	
 	
 	---------------------------------------------------------------------------------------
@@ -119,72 +161,182 @@ begin
 		elsif op = "0000011" and funct3 = "010" then						-- LW --
 		
 			PCSrc1 <= "00";
-			ResultSrc1 <= "01";
+			ResultSrc1 <= "001";
 			MemWrite1 <= '0';
-			ALUControl1 <= "000";
+			ALUControl1 <= "0000";
 			ALUSrc1 <= '1';
 			ImmSrc1 <= "000"; 
 			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
+		
+		elsif op = "0000011" and funct3 = "000" then						-- LB --
+		
+			PCSrc1 <= "00";
+			ResultSrc1 <= "001";
+			MemWrite1 <= '0';
+			ALUControl1 <= "0000";
+			ALUSrc1 <= '1';
+			ImmSrc1 <= "000"; 
+			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "10";
+		
+		elsif op = "0000011" and funct3 = "100" then						-- LBU --
+		
+			PCSrc1 <= "00";
+			ResultSrc1 <= "001";
+			MemWrite1 <= '0';
+			ALUControl1 <= "0000";
+			ALUSrc1 <= '1';
+			ImmSrc1 <= "000"; 
+			RegWrite1 <= '1';
+			DataType1 <= '1';
+			DataSize1 <= "10";
+		
+		elsif op = "0000011" and funct3 = "001" then						-- LH --
+		
+			PCSrc1 <= "00";
+			ResultSrc1 <= "001";
+			MemWrite1 <= '0';
+			ALUControl1 <= "0000";
+			ALUSrc1 <= '1';
+			ImmSrc1 <= "000"; 
+			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "01";
+		
+		elsif op = "0000011" and funct3 = "101" then						-- LHU --
+		
+			PCSrc1 <= "00";
+			ResultSrc1 <= "001";
+			MemWrite1 <= '0';
+			ALUControl1 <= "0000";
+			ALUSrc1 <= '1';
+			ImmSrc1 <= "000"; 
+			RegWrite1 <= '1';
+			DataType1 <= '1';
+			DataSize1 <= "01";
 		
 		elsif op = "1100111" and funct3 = "000" then						-- JALR --
 		
 			PCSrc1 <= "10";
-			ResultSrc1 <= "10";
+			ResultSrc1 <= "010";
 			MemWrite1 <= '0';
-			ALUControl1 <= "000";
+			ALUControl1 <= "0000";
 			ALUSrc1 <= '1';
 			ImmSrc1 <= "000"; 
 			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
 		
 		elsif op = "0010011" and funct3 = "000" then						-- ADDI --
 		
 			PCSrc1 <= "00";
-			ResultSrc1 <= "00";
+			ResultSrc1 <= "000";
 			MemWrite1 <= '0';
-			ALUControl1 <= "000";
+			ALUControl1 <= "0000";
 			ALUSrc1 <= '1';
 			ImmSrc1 <= "000"; 
 			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
 		
 		elsif op = "0010011" and funct3 = "111" then						-- ANDI --
 		
 			PCSrc1 <= "00";
-			ResultSrc1 <= "00";
+			ResultSrc1 <= "000";
 			MemWrite1 <= '0';
-			ALUControl1 <= "010";
+			ALUControl1 <= "0010";
 			ALUSrc1 <= '1';
 			ImmSrc1 <= "000"; 
 			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
 		
-		elsif op = "0010011" and funct3 = "001" then						-- SLLI --
+		elsif op = "0010011" and funct3 = "001" and funct7 = "0000000" then	-- SLLI --
 		
 			PCSrc1 <= "00";
-			ResultSrc1 <= "00";
+			ResultSrc1 <= "000";
 			MemWrite1 <= '0';
-			ALUControl1 <= "101";
+			ALUControl1 <= "0101";
 			ALUSrc1 <= '1';
 			ImmSrc1 <= "000"; 
 			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
 		
-		elsif op = "0010011" and funct3 = "101" then						-- SRLI --
+		elsif op = "0010011" and funct3 = "101" and funct7 = "0000000" then	-- SRLI --
 		
 			PCSrc1 <= "00";
-			ResultSrc1 <= "00";
+			ResultSrc1 <= "000";
 			MemWrite1 <= '0';
-			ALUControl1 <= "110";
+			ALUControl1 <= "0110";
 			ALUSrc1 <= '1';
 			ImmSrc1 <= "000"; 
 			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
 		
 		elsif op = "0010011" and funct3 = "010" then						-- SLTI --
 		
 			PCSrc1 <= "00";
-			ResultSrc1 <= "00";
+			ResultSrc1 <= "000";
 			MemWrite1 <= '0';
-			ALUControl1 <= "111";
+			ALUControl1 <= "0111";
 			ALUSrc1 <= '1';
 			ImmSrc1 <= "000"; 
 			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
+		
+		elsif op = "0010011" and funct3 = "110" then						-- ORI --
+		
+			PCSrc1 <= "00";
+			ResultSrc1 <= "000";
+			MemWrite1 <= '0';
+			ALUControl1 <= "0011";
+			ALUSrc1 <= '1';
+			ImmSrc1 <= "000"; 
+			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
+		
+		elsif op = "0010011" and funct3 = "100" then						-- XORI --
+		
+			PCSrc1 <= "00";
+			ResultSrc1 <= "000";
+			MemWrite1 <= '0';
+			ALUControl1 <= "0100";
+			ALUSrc1 <= '1';
+			ImmSrc1 <= "000"; 
+			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
+		
+		elsif op = "0010011" and funct3 = "101" and funct7 = "0100000" then -- SRAI --
+		
+			PCSrc1 <= "00";
+			ResultSrc1 <= "000";
+			MemWrite1 <= '0';
+			ALUControl1 <= "1001";
+			ALUSrc1 <= '1';
+			ImmSrc1 <= "101"; 
+			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
+		
+		elsif op = "0010011" and funct3 = "011" then						-- SLTUI --
+		
+			PCSrc1 <= "00";
+			ResultSrc1 <= "000";
+			MemWrite1 <= '0';
+			ALUControl1 <= "1000";
+			ALUSrc1 <= '1';
+			ImmSrc1 <= "000"; 
+			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
 		
 		
 		---------------------------------------------------------------------------------------
@@ -193,22 +345,74 @@ begin
 		elsif op = "1100011" and funct3 = "000" then						-- BEQ --
 		
 			PCSrc1 <= '0' & Zero;
-			ResultSrc1 <= "00";
+			ResultSrc1 <= "000";
 			MemWrite1 <= '0';
-			ALUControl1 <= "001";
+			ALUControl1 <= "0001";
 			ALUSrc1 <= '0';
 			ImmSrc1 <= "010"; 
 			RegWrite1 <= '0';
+			DataType1 <= '0';
+			DataSize1 <= "00";
 		
 		elsif op = "1100011" and funct3 = "001" then						-- BNE --
 		
 			PCSrc1 <= '0' & not Zero;
-			ResultSrc1 <= "00";
+			ResultSrc1 <= "000";
 			MemWrite1 <= '0';
-			ALUControl1 <= "001";
+			ALUControl1 <= "0001";
 			ALUSrc1 <= '0';
 			ImmSrc1 <= "010"; 
 			RegWrite1 <= '0';
+			DataType1 <= '0';
+			DataSize1 <= "00";
+		
+		elsif op = "1100011" and funct3 = "100" then						-- BLT --
+		
+			PCSrc1 <= '0' & (Negative xor Overflow);
+			ResultSrc1 <= "000";
+			MemWrite1 <= '0';
+			ALUControl1 <= "0001";
+			ALUSrc1 <= '0';
+			ImmSrc1 <= "010"; 
+			RegWrite1 <= '0';
+			DataType1 <= '0';
+			DataSize1 <= "00";
+		
+		elsif op = "1100011" and funct3 = "110" then						-- BLTU --
+		
+			PCSrc1 <= '0' & not Carry;
+			ResultSrc1 <= "000";
+			MemWrite1 <= '0';
+			ALUControl1 <= "0001";
+			ALUSrc1 <= '0';
+			ImmSrc1 <= "010"; 
+			RegWrite1 <= '0';
+			DataType1 <= '0';
+			DataSize1 <= "00";
+		
+		elsif op = "1100011" and funct3 = "101" then						-- BGE --
+		
+			PCSrc1 <= '0' & not(Negative xor Overflow);
+			ResultSrc1 <= "000";
+			MemWrite1 <= '0';
+			ALUControl1 <= "0001";
+			ALUSrc1 <= '0';
+			ImmSrc1 <= "010"; 
+			RegWrite1 <= '0';
+			DataType1 <= '0';
+			DataSize1 <= "00";
+			
+		elsif op = "1100011" and funct3 = "111" then						-- BGEU --
+		
+			PCSrc1 <= '0' & (Carry and (not Zero));
+			ResultSrc1 <= "000";
+			MemWrite1 <= '0';
+			ALUControl1 <= "0001";
+			ALUSrc1 <= '0';
+			ImmSrc1 <= "010"; 
+			RegWrite1 <= '0';
+			DataType1 <= '0';
+			DataSize1 <= "00";
 		
 		
 		---------------------------------------------------------------------------------------
@@ -217,12 +421,38 @@ begin
 		elsif op = "0100011" and funct3 = "010" then						-- SW --
 		
 			PCSrc1 <= "00";
-			ResultSrc1 <= "00";
+			ResultSrc1 <= "000";
 			MemWrite1 <= '1';
-			ALUControl1 <= "000";
+			ALUControl1 <= "0000";
 			ALUSrc1 <= '1';
 			ImmSrc1 <= "001"; 
 			RegWrite1 <= '0';
+			DataType1 <= '0';
+			DataSize1 <= "00";
+		
+		elsif op = "0100011" and funct3 = "001" then						-- SH --
+		
+			PCSrc1 <= "00";
+			ResultSrc1 <= "000";
+			MemWrite1 <= '1';
+			ALUControl1 <= "0000";
+			ALUSrc1 <= '1';
+			ImmSrc1 <= "001"; 
+			RegWrite1 <= '0';
+			DataType1 <= '0';
+			DataSize1 <= "01";
+		
+		elsif op = "0100011" and funct3 = "000" then						-- SB --
+		
+			PCSrc1 <= "00";
+			ResultSrc1 <= "000";
+			MemWrite1 <= '1';
+			ALUControl1 <= "0000";
+			ALUSrc1 <= '1';
+			ImmSrc1 <= "001"; 
+			RegWrite1 <= '0';
+			DataType1 <= '0';
+			DataSize1 <= "10";
 		
 		
 		---------------------------------------------------------------------------------------
@@ -231,12 +461,14 @@ begin
 		elsif op = "1101111"  then											-- JAL --
 		
 			PCSrc1 <= "01";
-			ResultSrc1 <= "10";
+			ResultSrc1 <= "010";
 			MemWrite1 <= '0';
-			ALUControl1 <= "000";
+			ALUControl1 <= "0000";
 			ALUSrc1 <= '0';
 			ImmSrc1 <= "100";
 			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
 			
 		
 		---------------------------------------------------------------------------------------
@@ -245,12 +477,26 @@ begin
 		elsif op = "0110111"  then											-- LUI --
 		
 			PCSrc1 <= "00";
-			ResultSrc1 <= "11";
+			ResultSrc1 <= "011";
 			MemWrite1 <= '0';
-			ALUControl1 <= "000";
+			ALUControl1 <= "0000";
 			ALUSrc1 <= '0';
 			ImmSrc1 <= "011";
 			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
+		
+		elsif op = "0010111"  then											-- AUIPC --
+		
+			PCSrc1 <= "00";
+			ResultSrc1 <= "100";
+			MemWrite1 <= '0';
+			ALUControl1 <= "0000";
+			ALUSrc1 <= '0';
+			ImmSrc1 <= "011";
+			RegWrite1 <= '1';
+			DataType1 <= '0';
+			DataSize1 <= "00";
 		
 		
 		---------------------------------------------------------------------------------------
@@ -258,12 +504,14 @@ begin
 		else
 		
 			PCSrc1 <= "00";
-			ResultSrc1 <= "00";
+			ResultSrc1 <= "000";
 			MemWrite1 <= '0';
-			ALUControl1 <= "000";
+			ALUControl1 <= "0000";
 			ALUSrc1 <= '0';
 			ImmSrc1 <= "000";
 			RegWrite1 <= '0';
+			DataType1 <= '0';
+			DataSize1 <= "00";
 		
 		end if;
 		
@@ -277,6 +525,8 @@ begin
 	ResultSrc <= ResultSrc1;
 	ALUControl <= ALUControl1;
 	ImmSrc <= ImmSrc1;
+	DataType <= DataType1;
+	DataSize <= DataSize1;
 
 
 end behave;
--- Control Unit/control_tb.vhd
+++ Control Unit/control_tb.vhd
@@ -7,13 +7,14 @@ end entity;
 
 architecture testbench of control_tb is
 	
-	signal MemWrite_tb, ALUSrc_tb, RegWrite_tb : std_logic;
-	signal ResultSrc_tb, PCSrc_tb : std_logic_vector(1 downto 0);
-	signal ALUControl_tb, ImmSrc_tb : std_logic_vector(2 downto 0);
+	signal MemWrite_tb, ALUSrc_tb, RegWrite_tb, DataType_tb : std_logic;
+	signal PCSrc_tb, DataSize_tb : std_logic_vector(1 downto 0);
+	signal ImmSrc_tb, ResultSrc_tb : std_logic_vector(2 downto 0);
 	signal op_tb : std_logic_vector(6 downto 0);
 	signal funct3_tb : std_logic_vector(14 downto 12);
 	signal funct7_tb : std_logic_vector(6 downto 0);
-	signal Zero_tb : std_logic;	
+	signal Zero_tb, Carry_tb, Overflow_tb, Negative_tb : std_logic;
+	signal ALUControl_tb : std_logic_vector(3 downto 0);
 	
 begin
 
@@ -24,13 +25,18 @@ begin
 			funct3 => funct3_tb,
 			funct7 => funct7_tb,
 			Zero => Zero_tb,
+			Carry => Carry_tb,
+			Overflow => Overflow_tb,
+			Negative => Negative_tb,
 			MemWrite => MemWrite_tb,
 			ALUSrc => ALUSrc_tb,
 			RegWrite => RegWrite_tb,
 			ResultSrc => ResultSrc_tb,
 			PCSrc => PCSrc_tb,
 			ALUControl => ALUControl_tb,
-			ImmSrc => ImmSrc_tb
+			ImmSrc => ImmSrc_tb,
+			DataSize => DataSize_tb,
+			DataType => DataType_tb
 		);
 	
 	process begin
@@ -40,7 +46,7 @@ begin
 		-- Unknown Command --
 		op_tb <= "0000000"; funct3_tb <= "000"; funct7_tb <= "0000000"; wait for 10 ns;
 		
-		assert PCSrc_tb = "00" and ResultSrc_tb = "00" and MemWrite_tb = '0' and ALUControl_tb = "000" and ALUSrc_tb = '0' and ImmSrc_tb = "000" and RegWrite_tb = '0'
+		assert PCSrc_tb = "00" and ResultSrc_tb = "000" and MemWrite_tb = '0' and ALUControl_tb = "0000" and ALUSrc_tb = '0' and ImmSrc_tb = "000" and RegWrite_tb = '0' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 1: Failed";
 		wait for 10 ns;
 		
@@ -48,56 +54,56 @@ begin
 		-- ADD --
 		op_tb <= "0110011"; funct3_tb <= "000"; funct7_tb <= "0000000"; wait for 10 ns;
 		
-		assert PCSrc_tb = "00" and ResultSrc_tb = "00" and MemWrite_tb = '0' and ALUControl_tb = "000" and ALUSrc_tb = '0' and ImmSrc_tb = "111" and RegWrite_tb = '1'
+		assert PCSrc_tb = "00" and ResultSrc_tb = "000" and MemWrite_tb = '0' and ALUControl_tb = "0000" and ALUSrc_tb = '0' and ImmSrc_tb = "111" and RegWrite_tb = '1' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 2: Failed";
 		wait for 10 ns;
 		
 		-- SUB --
 		op_tb <= "0110011"; funct3_tb <= "000"; funct7_tb <= "0100000"; wait for 10 ns;
 		
-		assert PCSrc_tb = "00" and ResultSrc_tb = "00" and MemWrite_tb = '0' and ALUControl_tb = "001" and ALUSrc_tb = '0' and ImmSrc_tb = "111" and RegWrite_tb = '1'
+		assert PCSrc_tb = "00" and ResultSrc_tb = "000" and MemWrite_tb = '0' and ALUControl_tb = "0001" and ALUSrc_tb = '0' and ImmSrc_tb = "111" and RegWrite_tb = '1' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 3: Failed";
 		wait for 10 ns;
 		
 		-- AND --
 		op_tb <= "0110011"; funct3_tb <= "111"; funct7_tb <= "0000000"; wait for 10 ns;
 		
-		assert PCSrc_tb = "00" and ResultSrc_tb = "00" and MemWrite_tb = '0' and ALUControl_tb = "010" and ALUSrc_tb = '0' and ImmSrc_tb = "111" and RegWrite_tb = '1'
+		assert PCSrc_tb = "00" and ResultSrc_tb = "000" and MemWrite_tb = '0' and ALUControl_tb = "0010" and ALUSrc_tb = '0' and ImmSrc_tb = "111" and RegWrite_tb = '1' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 4: Failed";
 		wait for 10 ns;
 		
 		-- OR --
 		op_tb <= "0110011"; funct3_tb <= "110"; funct7_tb <= "0000000"; wait for 10 ns;
 		
-		assert PCSrc_tb = "00" and ResultSrc_tb = "00" and MemWrite_tb = '0' and ALUControl_tb = "011" and ALUSrc_tb = '0' and ImmSrc_tb = "111" and RegWrite_tb = '1'
+		assert PCSrc_tb = "00" and ResultSrc_tb = "000" and MemWrite_tb = '0' and ALUControl_tb = "0011" and ALUSrc_tb = '0' and ImmSrc_tb = "111" and RegWrite_tb = '1' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 5: Failed";
 		wait for 10 ns;
 		
 		-- XOR --
 		op_tb <= "0110011"; funct3_tb <= "100"; funct7_tb <= "0000000"; wait for 10 ns;
 		
-		assert PCSrc_tb = "00" and ResultSrc_tb = "00" and MemWrite_tb = '0' and ALUControl_tb = "100" and ALUSrc_tb = '0' and ImmSrc_tb = "111" and RegWrite_tb = '1'
+		assert PCSrc_tb = "00" and ResultSrc_tb = "000" and MemWrite_tb = '0' and ALUControl_tb = "0100" and ALUSrc_tb = '0' and ImmSrc_tb = "111" and RegWrite_tb = '1' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 6: Failed";
 		wait for 10 ns;
 		
 		-- SLL --
 		op_tb <= "0110011"; funct3_tb <= "001"; funct7_tb <= "0000000"; wait for 10 ns;
 		
-		assert PCSrc_tb = "00" and ResultSrc_tb = "00" and MemWrite_tb = '0' and ALUControl_tb = "101" and ALUSrc_tb = '0' and ImmSrc_tb = "111" and RegWrite_tb = '1'
+		assert PCSrc_tb = "00" and ResultSrc_tb = "000" and MemWrite_tb = '0' and ALUControl_tb = "0101" and ALUSrc_tb = '0' and ImmSrc_tb = "111" and RegWrite_tb = '1' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 7: Failed";
 		wait for 10 ns;
 		
 		-- SRL --
 		op_tb <= "0110011"; funct3_tb <= "101"; funct7_tb <= "0000000"; wait for 10 ns;
 		
-		assert PCSrc_tb = "00" and ResultSrc_tb = "00" and MemWrite_tb = '0' and ALUControl_tb = "110" and ALUSrc_tb = '0' and ImmSrc_tb = "111" and RegWrite_tb = '1'
+		assert PCSrc_tb = "00" and ResultSrc_tb = "000" and MemWrite_tb = '0' and ALUControl_tb = "0110" and ALUSrc_tb = '0' and ImmSrc_tb = "111" and RegWrite_tb = '1' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 8: Failed";
 		wait for 10 ns;
 		
 		-- SLT --
 		op_tb <= "0110011"; funct3_tb <= "010"; funct7_tb <= "0000000"; wait for 10 ns;
 		
-		assert PCSrc_tb = "00" and ResultSrc_tb = "00" and MemWrite_tb = '0' and ALUControl_tb = "111" and ALUSrc_tb = '0' and ImmSrc_tb = "111" and RegWrite_tb = '1'
+		assert PCSrc_tb = "00" and ResultSrc_tb = "000" and MemWrite_tb = '0' and ALUControl_tb = "0111" and ALUSrc_tb = '0' and ImmSrc_tb = "111" and RegWrite_tb = '1' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 9: Failed";
 		wait for 10 ns;
 
@@ -105,49 +111,49 @@ begin
 		-- LW --
 		op_tb <= "0000011"; funct3_tb <= "010"; wait for 10 ns;
 		
-		assert PCSrc_tb = "00" and ResultSrc_tb = "01" and MemWrite_tb = '0' and ALUControl_tb = "000" and ALUSrc_tb = '1' and ImmSrc_tb = "000" and RegWrite_tb = '1'
+		assert PCSrc_tb = "00" and ResultSrc_tb = "001" and MemWrite_tb = '0' and ALUControl_tb = "0000" and ALUSrc_tb = '1' and ImmSrc_tb = "000" and RegWrite_tb = '1' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 10: Failed";
 		wait for 10 ns;
 		
 		-- JALR --
 		op_tb <= "1100111"; funct3_tb <= "000"; wait for 10 ns;
 		
-		assert PCSrc_tb = "10" and ResultSrc_tb = "10" and MemWrite_tb = '0' and ALUControl_tb = "000" and ALUSrc_tb = '1' and ImmSrc_tb = "000" and RegWrite_tb = '1'
+		assert PCSrc_tb = "10" and ResultSrc_tb = "010" and MemWrite_tb = '0' and ALUControl_tb = "0000" and ALUSrc_tb = '1' and ImmSrc_tb = "000" and RegWrite_tb = '1' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 11: Failed";
 		wait for 10 ns;
 		
 		-- ADDI --
 		op_tb <= "0010011"; funct3_tb <= "000"; wait for 10 ns;
 		
-		assert PCSrc_tb = "00" and ResultSrc_tb = "00" and MemWrite_tb = '0' and ALUControl_tb = "000" and ALUSrc_tb = '1' and ImmSrc_tb = "000" and RegWrite_tb = '1'
+		assert PCSrc_tb = "00" and ResultSrc_tb = "000" and MemWrite_tb = '0' and ALUControl_tb = "0000" and ALUSrc_tb = '1' and ImmSrc_tb = "000" and RegWrite_tb = '1' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 12: Failed";
 		wait for 10 ns;
 		
 		-- ANDI --
 		op_tb <= "0010011"; funct3_tb <= "111"; wait for 10 ns;
 		
-		assert PCSrc_tb = "00" and ResultSrc_tb = "00" and MemWrite_tb = '0' and ALUControl_tb = "010" and ALUSrc_tb = '1' and ImmSrc_tb = "000" and RegWrite_tb = '1'
+		assert PCSrc_tb = "00" and ResultSrc_tb = "000" and MemWrite_tb = '0' and ALUControl_tb = "0010" and ALUSrc_tb = '1' and ImmSrc_tb = "000" and RegWrite_tb = '1' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 13: Failed";
 		wait for 10 ns;
 		
 		-- SLLI --
 		op_tb <= "0010011"; funct3_tb <= "001"; wait for 10 ns;
 		
-		assert PCSrc_tb = "00" and ResultSrc_tb = "00" and MemWrite_tb = '0' and ALUControl_tb = "101" and ALUSrc_tb = '1' and ImmSrc_tb = "000" and RegWrite_tb = '1'
+		assert PCSrc_tb = "00" and ResultSrc_tb = "000" and MemWrite_tb = '0' and ALUControl_tb = "0101" and ALUSrc_tb = '1' and ImmSrc_tb = "000" and RegWrite_tb = '1' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 14: Failed";
 		wait for 10 ns;
 		
 		-- SRLI --
 		op_tb <= "0010011"; funct3_tb <= "101"; wait for 10 ns;
 		
-		assert PCSrc_tb = "00" and ResultSrc_tb = "00" and MemWrite_tb = '0' and ALUControl_tb = "110" and ALUSrc_tb = '1' and ImmSrc_tb = "000" and RegWrite_tb = '1'
+		assert PCSrc_tb = "00" and ResultSrc_tb = "000" and MemWrite_tb = '0' and ALUControl_tb = "0110" and ALUSrc_tb = '1' and ImmSrc_tb = "000" and RegWrite_tb = '1' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 15: Failed";
 		wait for 10 ns;
 		
 		-- SLTI --
 		op_tb <= "0010011"; funct3_tb <= "010"; wait for 10 ns;
 		
-		assert PCSrc_tb = "00" and ResultSrc_tb = "00" and MemWrite_tb = '0' and ALUControl_tb = "111" and ALUSrc_tb = '1' and ImmSrc_tb = "000" and RegWrite_tb = '1'
+		assert PCSrc_tb = "00" and ResultSrc_tb = "000" and MemWrite_tb = '0' and ALUControl_tb = "0111" and ALUSrc_tb = '1' and ImmSrc_tb = "000" and RegWrite_tb = '1' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 16: Failed";
 		wait for 10 ns;
 		
@@ -155,28 +161,28 @@ begin
 		-- BEQ (Zero = 0) --
 		op_tb <= "1100011"; funct3_tb <= "000"; Zero_tb <= '0'; wait for 10 ns;
 		
-		assert PCSrc_tb = "00" and ResultSrc_tb = "00" and MemWrite_tb = '0' and ALUControl_tb = "001" and ALUSrc_tb = '0' and ImmSrc_tb = "010" and RegWrite_tb = '0'
+		assert PCSrc_tb = "00" and ResultSrc_tb = "000" and MemWrite_tb = '0' and ALUControl_tb = "0001" and ALUSrc_tb = '0' and ImmSrc_tb = "010" and RegWrite_tb = '0' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 17: Failed";
 		wait for 10 ns;
 		
 		-- BEQ (Zero = 1)  --
 		op_tb <= "1100011"; funct3_tb <= "000"; Zero_tb <= '1'; wait for 10 ns;
 		
-		assert PCSrc_tb = "01" and ResultSrc_tb = "00" and MemWrite_tb = '0' and ALUControl_tb = "001" and ALUSrc_tb = '0' and ImmSrc_tb = "010" and RegWrite_tb = '0'
+		assert PCSrc_tb = "01" and ResultSrc_tb = "000" and MemWrite_tb = '0' and ALUControl_tb = "0001" and ALUSrc_tb = '0' and ImmSrc_tb = "010" and RegWrite_tb = '0' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 18: Failed";
 		wait for 10 ns;
 		
 		-- BNE (Zero = 0) --
 		op_tb <= "1100011"; funct3_tb <= "001"; Zero_tb <= '0'; wait for 10 ns;
 		
-		assert PCSrc_tb = "01" and ResultSrc_tb = "00" and MemWrite_tb = '0' and ALUControl_tb = "001" and ALUSrc_tb = '0' and ImmSrc_tb = "010" and RegWrite_tb = '0'
+		assert PCSrc_tb = "01" and ResultSrc_tb = "000" and MemWrite_tb = '0' and ALUControl_tb = "0001" and ALUSrc_tb = '0' and ImmSrc_tb = "010" and RegWrite_tb = '0' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 19: Failed";
 		wait for 10 ns;
 		
 		-- BNE (Zero = 1)  --
 		op_tb <= "1100011"; funct3_tb <= "001"; Zero_tb <= '1'; wait for 10 ns;
 		
-		assert PCSrc_tb = "00" and ResultSrc_tb = "00" and MemWrite_tb = '0' and ALUControl_tb = "001" and ALUSrc_tb = '0' and ImmSrc_tb = "010" and RegWrite_tb = '0'
+		assert PCSrc_tb = "00" and ResultSrc_tb = "000" and MemWrite_tb = '0' and ALUControl_tb = "0001" and ALUSrc_tb = '0' and ImmSrc_tb = "010" and RegWrite_tb = '0' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 20: Failed";
 		wait for 10 ns;
 	---------------------------------------------------------------------------------------------------------------------------------------------------------------------------	
@@ -184,7 +190,7 @@ begin
 		-- SW --
 		op_tb <= "0100011"; funct3_tb <= "010"; wait for 10 ns;
 		
-		assert PCSrc_tb = "00" and ResultSrc_tb = "00" and MemWrite_tb = '1' and ALUControl_tb = "000" and ALUSrc_tb = '1' and ImmSrc_tb = "001" and RegWrite_tb = '0'
+		assert PCSrc_tb = "00" and ResultSrc_tb = "000" and MemWrite_tb = '1' and ALUControl_tb = "0000" and ALUSrc_tb = '1' and ImmSrc_tb = "001" and RegWrite_tb = '0' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 21: Failed";
 		wait for 10 ns;
 		
@@ -193,7 +199,7 @@ begin
 		-- JAL --
 		op_tb <= "1101111"; wait for 10 ns;
 		
-		assert PCSrc_tb = "01" and ResultSrc_tb = "10" and MemWrite_tb = '0' and ALUControl_tb = "000" and ALUSrc_tb = '0' and ImmSrc_tb = "100" and RegWrite_tb = '1'
+		assert PCSrc_tb = "01" and ResultSrc_tb = "010" and MemWrite_tb = '0' and ALUControl_tb = "0000" and ALUSrc_tb = '0' and ImmSrc_tb = "100" and RegWrite_tb = '1' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 22: Failed";
 		wait for 10 ns;
 		
@@ -202,9 +208,10 @@ begin
 		-- LUI --
 		op_tb <= "0110111"; wait for 10 ns;
 		
-		assert PCSrc_tb = "00" and ResultSrc_tb = "11" and MemWrite_tb = '0' and ALUControl_tb = "000" and ALUSrc_tb = '0' and ImmSrc_tb = "011" and RegWrite_tb = '1'
+		assert PCSrc_tb = "00" and ResultSrc_tb = "011" and MemWrite_tb = '0' and ALUControl_tb = "0000" and ALUSrc_tb = '0' and ImmSrc_tb = "011" and RegWrite_tb = '1' and DataType_tb <= '0' and DataSize_tb <= "00"
 		report "Case 23: Failed";
+		
+		report "Test bench complete.";
 		wait;
-	
 	end process;
 end testbench;
